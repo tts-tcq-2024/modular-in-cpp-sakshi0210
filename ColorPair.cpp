9ec5fdbd6301055a1488d78fe3c85f07b718badf
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-30 14:30:11 +0530

    Create ColorPair.cpp

---
+++ ColorPair.cpp
@@ -0,0 +1,50 @@
+#include "ColorPair.h"
+#include <sstream>
+
+namespace TelCoColorCoder {
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    ColorPair::ColorPair(MajorColor major, MinorColor minor)
+        : majorColor(major), minorColor(minor) {}
+
+    MajorColor ColorPair::getMajor() const {
+        return majorColor;
+    }
+
+    MinorColor ColorPair::getMinor() const {
+        return minorColor;
+    }
+
+    std::string ColorPair::ToString() const {
+        return std::string(MajorColorNames[majorColor]) + " " + MinorColorNames[minorColor];
+    }
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = static_cast<MajorColor>(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = static_cast<MinorColor>(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+
+    std::string GetColorCodingManual() {
+        std::ostringstream oss;
+        for (int i = 0; i < numberOfMajorColors; ++i) {
+            for (int j = 0; j < numberOfMinorColors; ++j) {
+                oss << GetPairNumberFromColor(static_cast<MajorColor>(i), static_cast<MinorColor>(j))
+                    << ": " << MajorColorNames[i] << " " << MinorColorNames[j] << "\n";
+            }
+        }
+        return oss.str();
+    }
+}
